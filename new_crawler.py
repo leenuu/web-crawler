d56f7e5bbb1c764b82beeea49bda96a4584965e3
leenuu <61344683+leenuu@users.noreply.github.com>
2020-07-14 13:42:05 +0900

    Update new_crawler.py

--- new_crawler.py
+++ new_crawler.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
-# from multiprocessing import Pool
+from multiprocessing import Pool
 import os
 import requests
 import webbrowser
@@ -10,8 +10,6 @@ import time
 import random
 
 _src = ''
-all_prg = 0
-com_prg = 0
 
 def ch():
     ua = UserAgent()
@@ -22,7 +20,7 @@ def ch():
     print(html)
 
 
-def get_link(src):
+def get_link_google(src):
     print('start get links...')
     ua = UserAgent()
     header = {'User-Agent':str(ua.chrome)}
@@ -33,22 +31,56 @@ def get_link(src):
     html = req_html.text
     soup = BeautifulSoup(html, 'html.parser')
 
-    url_data = list()
-    url_data.append('/search?q=' + src)
+    url_google_data = list()
+    url_google_data.append('/search?q=' + src)
     for _url in soup.find('div', id="foot", role="navigation") and soup.find('table', class_='AaVjTc') and soup.find_all('td') and soup.find_all('a', class_="fl") and soup.find_all('span', class_="SJajHc NVbCr"):
-        url_data.append(_url.parent.get('href'))
-    del url_data[len(url_data)-1]
+        url_google_data.append(_url.parent.get('href'))
+    del url_google_data[len(url_google_data)-1]
+    print('complete get links..')
+    return url_google_data
+
+def get_rsc_google(link):
+    print('start get rsc...')
+    ua = UserAgent()
+    header = {'User-Agent':str(ua.chrome)}
+    _url = 'https://www.google.com' + link
+
+    rand_value = random.uniform(0,5)
+    time.sleep(rand_value)
+
+    print('requests...')
+    req = requests.get(_url, headers=header)
+    html = req.text
+    soup = BeautifulSoup(html, 'html.parser')
+
+    rsc_name_data = list()
+    rsc_url_data = list()
+    rsc_google_data = dict()
+    for link in soup.find_all("div", class_ = 'r') and soup.find_all('a') and soup.find_all('h3'):
+        rsc_url_data.append(link.parent.get('href'))
+        rsc_name_data.append(link.get_text())
+            
+    for num in range(0,len(rsc_name_data)):
+        rsc_google_data[rsc_name_data[num]] = rsc_url_data[num]
+    print('complete get src..')
+    return rsc_google_data
+
+def get_link_naver(src):
+    url_data = list()
+    print('start get links...')
+    for num in range(0,11):
+        page = 1 + num * 10 
+        url_data.append(f'https://search.naver.com/search.naver?sm=tab_hty.top&where=post&query={src}&where=post&start={page}')
     print('complete get links..')
     return url_data
 
-def get_rsc(link):
-    global com_prg
+def get_rsc_naver(link):
     print('start get rsc...')
     ua = UserAgent()
     header = {'User-Agent':str(ua.chrome)}
-    _url = 'https://www.google.com' + link
+    _url = link
 
-    rand_value = random.uniform(0,5)
+    rand_value = random.uniform(0,12)
     time.sleep(rand_value)
 
     print('requests...')
@@ -58,77 +90,54 @@ def get_rsc(link):
 
     rsc_name_data = list()
     rsc_url_data = list()
-    rsc_data = dict()
-    for link in soup.find_all("div", class_ = 'r') and soup.find_all('a') and soup.find_all('h3'):
-        rsc_url_data.append(link.parent.get('href'))
-        rsc_name_data.append(link.get_text())
+    rsc_naver_data = dict()
+    for link in soup.find("div", class_="blog section _blogBase _prs_blg") and soup.find('ul', id="elThumbnailResultArea") and soup.find_all('li', class_='sh_blog_top') and soup.find_all('a'):
+        if _src in link.get_text() and '?where=post&query=' not in link.get('href'): 
+            rsc_url_data.append(link.get('href'))
+            rsc_name_data.append(link.get_text())
             
     for num in range(0,len(rsc_name_data)):
-        rsc_data[rsc_name_data[num]] = rsc_url_data[num]
-    com_prg += 1
+        rsc_naver_data[rsc_name_data[num]] = rsc_url_data[num]
+    
     print('complete get src..')
-    print(f'{com_prg}/{all_prg}')
-    return rsc_data
-
-def get_rsc_pool(link):
-    print('start get rsc...')
-    ua = UserAgent()
-    header = {'User-Agent':str(ua.chrome)}
-    _url = 'https://www.google.com' + link
-
-    rand_value = random.uniform(0,5)
-    time.sleep(rand_value)
-
-    print('requests...')
-    req = requests.get(_url, headers=header)
-    html = req.text
-    soup = BeautifulSoup(html, 'html.parser')
-
-    rsc_name_data = list()
-    rsc_url_data = list()
-    rsc_data = dict()
-    for link in soup.find_all("div", class_ = 'r') and soup.find_all('a') and soup.find_all('h3'):
-        rsc_url_data.append(link.parent.get('href'))
-        rsc_name_data.append(link.get_text())
-            
-    for num in range(0,len(rsc_name_data)):
-        rsc_data[rsc_name_data[num]] = rsc_url_data[num]
-
-    print('complete get src..')
-    return rsc_data
+    return rsc_naver_data
 
 def res():
     if __name__ == '__main__':
-        global all_prg
-        global com_prg
+        global _src
         _src = input("Please enter here to search: ") 
         if _src == ' ' or _src == '' or _src == 'stop()':
             return print('stop')
 
         del_st = '관련 검색: ' + _src
-        links = get_link(_src)
-        print(f"Total pages: {len(links)}")
-        all_prg = len(links)
+        links_naver = get_link_naver(_src)
+        links_google = get_link_google(_src)
+        print(f"Total pages naver: {len(links_naver)}")
+        print(f"Total pages google: {len(links_google)}")
         
         s_t = time.time()
         result = dict()
-        data = list(map(get_rsc,links[:len(links)]))
-
-        # pool = Pool(processes=3)
-        # data += pool.map(get_rsc_pool, links[:len(links)])
+        # data_naver = list(map(get_rsc_naver,links_naver[:len(links_naver)]))
+        # data_google = list(map(get_rsc_google,links_google[:len(links_google)]))
+        pool = Pool(processes=3)
+        data = list()
+        print('start naver search')
+        data += pool.map(get_rsc_naver,links_naver[:len(links_naver)])
+        print('complete naver search')
+        print('start google search')
+        data += pool.map(get_rsc_google,links_google[:len(links_google)])
+        print('complete google search')
 
         print('sum result..')
         for num in range(0,len(data)):
             for i in data[num]:
                 if i != del_st and _src in i and data[num][i] != None:
                     result[i] = data[num][i]
-
+        
         e_t = time.time()
         print('result..')
         print(f"{e_t - s_t} s...")
         print(result)
-        all_prg = 0
-        com_prg = 0
         # return result
 
 res()

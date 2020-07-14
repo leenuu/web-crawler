78c22c37d20c349725f7076831036419fc91087d
leenuu <61344683+leenuu@users.noreply.github.com>
2020-07-14 16:42:45 +0900

    Update new_crawler.py

--- new_crawler.py
+++ new_crawler.py
@@ -10,6 +10,8 @@ import time
 import random
 
 _src = ''
+de_time = 40
+processes_num = 3
 
 def ch():
     ua = UserAgent()
@@ -29,7 +31,11 @@ def get_link_google(src):
     print('requests...')
     req_html = requests.get(url, headers=header)
     html = req_html.text
+    if html == None:
+        print('requests error \a')
+        return 0
     soup = BeautifulSoup(html, 'html.parser')
+    
 
     url_google_data = list()
     url_google_data.append('/search?q=' + src)
@@ -45,12 +51,15 @@ def get_rsc_google(link):
     header = {'User-Agent':str(ua.chrome)}
     _url = 'https://www.google.com' + link
 
-    rand_value = random.uniform(0,5)
+    rand_value = random.uniform(0,de_time)
     time.sleep(rand_value)
 
     print('requests...')
     req = requests.get(_url, headers=header)
     html = req.text
+    if html == None:
+        print('requests error \a')
+        return 0
     soup = BeautifulSoup(html, 'html.parser')
 
     rsc_name_data = list()
@@ -66,13 +75,13 @@ def get_rsc_google(link):
     return rsc_google_data
 
 def get_link_naver(src):
-    url_data = list()
+    url_naver_data = list()
     print('start get links...')
-    for num in range(0,11):
+    for num in range(0,10):
         page = 1 + num * 10 
-        url_data.append(f'https://search.naver.com/search.naver?sm=tab_hty.top&where=post&query={src}&where=post&start={page}')
+        url_naver_data.append(f'https://search.naver.com/search.naver?sm=tab_hty.top&where=post&query={src}&where=post&start={page}')
     print('complete get links..')
-    return url_data
+    return url_naver_data
 
 def get_rsc_naver(link):
     print('start get rsc...')
@@ -80,12 +89,15 @@ def get_rsc_naver(link):
     header = {'User-Agent':str(ua.chrome)}
     _url = link
 
-    rand_value = random.uniform(0,12)
+    rand_value = random.uniform(0,de_time)
     time.sleep(rand_value)
 
     print('requests...')
     req = requests.get(_url, headers=header)
     html = req.text
+    if html == None:
+        print('requests error \a')
+        return 0
     soup = BeautifulSoup(html, 'html.parser')
 
     rsc_name_data = list()
@@ -102,32 +114,90 @@ def get_rsc_naver(link):
     print('complete get src..')
     return rsc_naver_data
 
+def get_link_daum(src):
+    url_data = list()
+    print('start get links...')
+    for page in range(1,11):
+        url_data.append(f'https://search.daum.net/search?w=blog&DA=PGD&enc=utf8&q={src}&page={page}')
+    print('complete get links..')
+    return url_data
+
+def get_rsc_daum(link):
+    print('start get rsc...')
+    ua = UserAgent()
+    header = {'User-Agent':str(ua.chrome)}
+    _url = link
+
+    rand_value = random.uniform(0,de_time)
+    time.sleep(rand_value)
+
+    print('requests...')
+    req = requests.get(_url, headers=header)
+    html = req.text
+    if html == None:
+        print('requests error \a')
+        return 0
+    soup = BeautifulSoup(html, 'html.parser')
+
+    rsc_name_data = list()
+    rsc_url_data = list()
+    rsc_daum_data = dict()
+    for link in soup.find("div", id='blogColl') and soup.find('ul', class_="list_info mg_cont clear") and soup.find_all('li') and soup.find_all('div', class_="wrap_tit mg_tit") and soup.find_all('a', class_='f_link_b'):
+        if _src in link.get_text(): 
+            rsc_url_data.append(link.get('href'))
+            rsc_name_data.append(link.get_text())
+            
+    for num in range(0,len(rsc_name_data)):
+        rsc_daum_data[rsc_name_data[num]] = rsc_url_data[num]
+    
+    print('complete get src..')
+    return rsc_daum_data
+
 def res():
     if __name__ == '__main__':
         global _src
         _src = input("Please enter here to search: ") 
+        s_t = time.time()
         if _src == ' ' or _src == '' or _src == 'stop()':
             return print('stop')
 
         del_st = '관련 검색: ' + _src
-        links_naver = get_link_naver(_src)
         links_google = get_link_google(_src)
-        print(f"Total pages naver: {len(links_naver)}")
-        print(f"Total pages google: {len(links_google)}")
+        links_naver = get_link_naver(_src)
+        links_daum = get_link_daum(_src)
         
-        s_t = time.time()
+        print(f'delay req: {de_time}')
+        print(f'Total processes: {processes_num}')
+        print(f"Total pages google: {len(links_google)}")
+        print(f"Total pages naver: {len(links_naver)}")
+        print(f"Total pages daum: {len(links_daum)}")
+        
         result = dict()
         # data_naver = list(map(get_rsc_naver,links_naver[:len(links_naver)]))
         # data_google = list(map(get_rsc_google,links_google[:len(links_google)]))
-        pool = Pool(processes=3)
+        pool = Pool(processes=processes_num)
         data = list()
-        print('start naver search')
-        data += pool.map(get_rsc_naver,links_naver[:len(links_naver)])
-        print('complete naver search')
-        print('start google search')
-        data += pool.map(get_rsc_google,links_google[:len(links_google)])
-        print('complete google search')
+        try:
+            print('start google search')
+            data += pool.map(get_rsc_google,links_google[:len(links_google)])
+            print('complete google search')
+        except TypeError:
+            print('requests error \a')
 
+        try:
+            print('start naver search')
+            data += pool.map(get_rsc_naver,links_naver[:len(links_naver)])
+            print('complete naver search')
+        except TypeError:
+            print('requests error \a')
+
+        try:
+            print('start daum search')
+            data += pool.map(get_rsc_daum,links_daum[:len(links_daum)])
+            print('complete daum search')
+        except TypeError:
+            print('requests error \a')
+         
         print('sum result..')
         for num in range(0,len(data)):
             for i in data[num]:
@@ -136,8 +206,9 @@ def res():
         
         e_t = time.time()
         print('result..')
-        print(f"{e_t - s_t} s...")
         print(result)
+        print(f"{e_t - s_t} s...")
+        print(len(result))
         # return result
 
 res()

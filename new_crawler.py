6940a3c8713425cb2b513e8fd65ea6bb101b15c8
leenuu <61344683+leenuu@users.noreply.github.com>
2020-07-14 11:54:58 +0900

    Update new_crawler.py

--- new_crawler.py
+++ new_crawler.py
@@ -2,12 +2,25 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
-from multiprocessing import Pool
+# from multiprocessing import Pool
 import os
 import requests
 import webbrowser
 import time
 import random
+
+_src = ''
+all_prg = 0
+com_prg = 0
+
+def ch():
+    ua = UserAgent()
+    header = {'User-Agent':str(ua.chrome)}
+    url = 'https://www.google.com/search?q=파이썬'
+    req_html = requests.get(url, headers=header)
+    html = req_html.text
+    print(html)
+
 
 def get_link(src):
     print('start get links...')
@@ -26,18 +39,16 @@ def get_link(src):
         url_data.append(_url.parent.get('href'))
     del url_data[len(url_data)-1]
     print('complete get links..')
-    # print(url_data)
     return url_data
 
 def get_rsc(link):
-    
+    global com_prg
     print('start get rsc...')
-
     ua = UserAgent()
     header = {'User-Agent':str(ua.chrome)}
     _url = 'https://www.google.com' + link
 
-    rand_value = random.uniform(0,30)
+    rand_value = random.uniform(0,5)
     time.sleep(rand_value)
 
     print('requests...')
@@ -45,41 +56,82 @@ def get_rsc(link):
     html = req.text
     soup = BeautifulSoup(html, 'html.parser')
 
-    rsc_data = list()
+    rsc_name_data = list()
+    rsc_url_data = list()
+    rsc_data = dict()
     for link in soup.find_all("div", class_ = 'r') and soup.find_all('a') and soup.find_all('h3'):
-        if link.get_text().find(_src) == 0:
-            # rsc_data.append(link.parent.get('href'))
-            rsc_data.append(link.get_text())
+        rsc_url_data.append(link.parent.get('href'))
+        rsc_name_data.append(link.get_text())
+            
+    for num in range(0,len(rsc_name_data)):
+        rsc_data[rsc_name_data[num]] = rsc_url_data[num]
+    com_prg += 1
+    print('complete get src..')
+    print(f'{com_prg}/{all_prg}')
+    return rsc_data
+
+def get_rsc_pool(link):
+    print('start get rsc...')
+    ua = UserAgent()
+    header = {'User-Agent':str(ua.chrome)}
+    _url = 'https://www.google.com' + link
+
+    rand_value = random.uniform(0,5)
+    time.sleep(rand_value)
+
+    print('requests...')
+    req = requests.get(_url, headers=header)
+    html = req.text
+    soup = BeautifulSoup(html, 'html.parser')
+
+    rsc_name_data = list()
+    rsc_url_data = list()
+    rsc_data = dict()
+    for link in soup.find_all("div", class_ = 'r') and soup.find_all('a') and soup.find_all('h3'):
+        rsc_url_data.append(link.parent.get('href'))
+        rsc_name_data.append(link.get_text())
+            
+    for num in range(0,len(rsc_name_data)):
+        rsc_data[rsc_name_data[num]] = rsc_url_data[num]
 
     print('complete get src..')
     return rsc_data
 
 def res():
     if __name__ == '__main__':
+        global all_prg
+        global com_prg
         _src = input("Please enter here to search: ") 
+        if _src == ' ' or _src == '' or _src == 'stop()':
+            return print('stop')
+
         del_st = '관련 검색: ' + _src
         links = get_link(_src)
-
-        data = list()
-        sum_data = list()
-
+        print(f"Total pages: {len(links)}")
+        all_prg = len(links)
+        
         s_t = time.time()
-        pool = Pool(processes=3)
-        data += pool.map(get_rsc, links[:len(links)])
+        result = dict()
+        data = list(map(get_rsc,links[:len(links)]))
+
+        # pool = Pool(processes=3)
+        # data += pool.map(get_rsc_pool, links[:len(links)])
+
         print('sum result..')
-        for i in range(0,len(data)):
-            for j in range(0,len(data[i])):
-                if data[i][j] == del_st: 
-                    pass
-                else:    
-                    sum_data.append(data[i][j])
+        for num in range(0,len(data)):
+            for i in data[num]:
+                if i != del_st and _src in i and data[num][i] != None:
+                    result[i] = data[num][i]
 
         e_t = time.time()
         print('result..')
         print(f"{e_t - s_t} s...")
-        print(len(data))
-        print(sum_data)
-    
-_src = ''
+        print(result)
+        all_prg = 0
+        com_prg = 0
+        # return result
 
 res()
+
+# ch()
+
